efc9c287875898058f0de33e347d7eff13f8fba1
ESRAILHAQUE <esrailbblhs@gmail.com>
2024-03-04 23:41:21 +0600

    searching bug fixed

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ let posts;
 const loadPosts = async (searchInput) => {
   try {
     const res = await fetch(
-      `https://openapi.programming-hero.com/api/retro-forum/posts?category=${searchInput}`
+      searchInput
+        ? `https://openapi.programming-hero.com/api/retro-forum/posts?category=${searchInput}`
+        : "https://openapi.programming-hero.com/api/retro-forum/posts"
     );
+
     const data = await res.json();
     posts = data.posts;
     console.log(posts);
